f20f8bf995cb138f1d29940ae80676896fc6e67f
rupeshsah86 <rupeshkumarsah.2024cse@sece.ac.in>
2025-10-19 11:51:32 +0530

    feat: initialize spring boot project with basic configuration

---
+++ src/main/java/com/ecommerce/returnmanager/ReturnRefundManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.returnmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnRefundManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnRefundManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/returnmanager/ReturnRefundManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.returnmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnRefundManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
